0bf0f621d0bf7ff56da5cdc4bb63379da3062ee4
Chaiwat Chairit <dogajack@gmail.com>
2026-05-02 12:40:39 +0700

    xgver

--- backend/app/utils/__init__.py
+++ backend/app/utils/__init__.py
@@ -1,9 +1,12 @@
-from datetime import datetime, timezone
+from datetime import datetime
+from zoneinfo import ZoneInfo
 import uuid
 from .uuid7 import generate_uuid7
+
+tz = ZoneInfo("Asia/Bangkok")
 
 def generate_uuid() -> uuid.UUID:
     return generate_uuid7()
 
 def now() -> datetime:
-    return datetime.now(timezone.utc)
+    return datetime.now(tz)
